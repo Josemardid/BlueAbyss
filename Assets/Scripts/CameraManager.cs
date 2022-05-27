d6cc00836ea3c22551c2de9885f30f184b94af1e
Josemardid <61411305+Josemardid@users.noreply.github.com>
2022-05-27 18:00:25 +0200

    Cams

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -9,6 +9,8 @@ public class CameraManager : MonoBehaviour
     private float elongation;
     private float daRealXinFormula;
 
+    private int camIndex = 0;
+
     public GameObject mainCamera;
 
     private float distBtnObjs;
@@ -20,7 +22,7 @@ public class CameraManager : MonoBehaviour
     public float distZ = 15;
     public float distY = 10;
    
-    public Transform cameraPosition;
+    public Transform [] cameraPosition;
 
     public bool outSphere = false;
 
@@ -28,9 +30,9 @@ public class CameraManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        cameraPosition.position = new Vector3(this.transform.position.x, this.transform.position.y + distY, this.transform.position.z - distZ);
+        cameraPosition[0].position = new Vector3(this.transform.position.x, this.transform.position.y + distY, this.transform.position.z - distZ);
         //elongation = radiusSphere;
-        mainCamera.transform.position = cameraPosition.position;
+        mainCamera.transform.position = cameraPosition[camIndex].position;
 
         //EmergencyK = k + EmergencyKAdd;
     }
@@ -39,77 +41,95 @@ public class CameraManager : MonoBehaviour
     public void Update()
     {
         //CameraClampSphere();
+        mainCamera.transform.position = cameraPosition[camIndex].position;
+        mainCamera.transform.LookAt(this.gameObject.transform);
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            ChangeCam();
+        }
     }
 
    
     void FixedUpdate()
     {
         //HookeMov();
-        mainCamera.transform.position = cameraPosition.position;
-        mainCamera.transform.LookAt(this.gameObject.transform);
+
 
         
     }
 
+    private void ChangeCam()
+    {
+        camIndex++;
+
+        if(camIndex > cameraPosition.Length-1)
+        {
+            camIndex = 0;
+        }
+
+    }
+
+
     private void OnDrawGizmos()
     {
-        Debug.DrawLine(cameraPosition.position, this.gameObject.transform.position, Color.green);
-        Gizmos.DrawSphere(cameraPosition.position, radiusSphere);
+        //Debug.DrawLine(cameraPosition[0].position, this.gameObject.transform.position, Color.green);
+        //Gizmos.DrawSphere(cameraPosition[0].position, radiusSphere);
     }
 
-    void HookeMov()
-    {
+//    void HookeMov()
+//    {
 
-        distBtnObjs = (mainCamera.transform.position - cameraPosition.position).magnitude;
+//        distBtnObjs = (mainCamera.transform.position - cameraPosition[0].position).magnitude;
 
 
-        daRealXinFormula = Mathf.Abs(distBtnObjs - elongation);
+//        daRealXinFormula = Mathf.Abs(distBtnObjs - elongation);
 
-        forceVec = (cameraPosition.position - mainCamera.transform.position).normalized * k * daRealXinFormula;
+//        forceVec = (cameraPosition[0].position - mainCamera.transform.position).normalized * k * daRealXinFormula;
 
-        if (!CamOutOfSphere())
-        {
-            mainCamera.GetComponent<Rigidbody>().AddForce(forceVec, ForceMode.Force);
-            k = EmergencyK - EmergencyKAdd;
+//        if (!CamOutOfSphere())
+//        {
+//            mainCamera.GetComponent<Rigidbody>().AddForce(forceVec, ForceMode.Force);
+//            k = EmergencyK - EmergencyKAdd;
 
-        }
-        else
-        {
-            k = EmergencyK;
-            //Vector3 toPosition = (cameraPosition.position - mainCamera.transform.position).normalized;
-            //No se arreglar la cam
-            //mainCamera.GetComponent<Rigidbody>().velocity =Vector3.zero;
-        }
+//        }
+//        else
+//        {
+//            k = EmergencyK;
+//            //Vector3 toPosition = (cameraPosition.position - mainCamera.transform.position).normalized;
+//            //No se arreglar la cam
+//            //mainCamera.GetComponent<Rigidbody>().velocity =Vector3.zero;
+//        }
        
-    }
-    void CameraClampSphere()
-    {
-        Vector3 newPositionCam;
-        newPositionCam.x = Mathf.Clamp(mainCamera.transform.position.x, cameraPosition.position.x - radiusSphere, cameraPosition.position.x + radiusSphere);
-        newPositionCam.y = Mathf.Clamp(mainCamera.transform.position.y, cameraPosition.position.y - radiusSphere, cameraPosition.position.y + radiusSphere);
-        newPositionCam.z = Mathf.Clamp(mainCamera.transform.position.z, cameraPosition.position.z - radiusSphere, cameraPosition.position.z + radiusSphere);
-        mainCamera.transform.position = newPositionCam;
+//    }
+//    void CameraClampSphere()
+//    {
+//        Vector3 newPositionCam;
+//        newPositionCam.x = Mathf.Clamp(mainCamera.transform.position.x, cameraPosition.position.x - radiusSphere, cameraPosition.position.x + radiusSphere);
+//        newPositionCam.y = Mathf.Clamp(mainCamera.transform.position.y, cameraPosition.position.y - radiusSphere, cameraPosition.position.y + radiusSphere);
+//        newPositionCam.z = Mathf.Clamp(mainCamera.transform.position.z, cameraPosition.position.z - radiusSphere, cameraPosition.position.z + radiusSphere);
+//        mainCamera.transform.position = newPositionCam;
 
-    }
-    bool CamOutOfSphere()
-    {
-        outSphere = false;
-        if (mainCamera.transform.position.x < cameraPosition.position.x - radiusSphere || mainCamera.transform.position.x > cameraPosition.position.x + radiusSphere)
-        {
-            outSphere = true;
-            Debug.Log("fuera de la esfera X");
-        }
-        if ( mainCamera.transform.position.y < cameraPosition.position.y - radiusSphere || mainCamera.transform.position.y > cameraPosition.position.y + radiusSphere)
-        {
-            outSphere = true;
-            Debug.Log("fuera de la esfera Y");
-        }
-        if ( mainCamera.transform.position.z < cameraPosition.position.z - radiusSphere || mainCamera.transform.position.z > cameraPosition.position.z + radiusSphere)
-        {
-            outSphere = true;
-            Debug.Log("fuera de la esfera Z");
-        }
-        return outSphere;
-    }
+//    }
+//    bool CamOutOfSphere()
+//    {
+//        outSphere = false;
+//        if (mainCamera.transform.position.x < cameraPosition.position.x - radiusSphere || mainCamera.transform.position.x > cameraPosition.position.x + radiusSphere)
+//        {
+//            outSphere = true;
+//            Debug.Log("fuera de la esfera X");
+//        }
+//        if ( mainCamera.transform.position.y < cameraPosition.position.y - radiusSphere || mainCamera.transform.position.y > cameraPosition.position.y + radiusSphere)
+//        {
+//            outSphere = true;
+//            Debug.Log("fuera de la esfera Y");
+//        }
+//        if ( mainCamera.transform.position.z < cameraPosition.position.z - radiusSphere || mainCamera.transform.position.z > cameraPosition.position.z + radiusSphere)
+//        {
+//            outSphere = true;
+//            Debug.Log("fuera de la esfera Z");
+//        }
+//        return outSphere;
+//    }
 }
 
