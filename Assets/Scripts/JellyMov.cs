f1e465ac14014152937330e777b714e1d985bd2b
kidtastrophe <verapb.com@gmail.com>
2022-05-31 18:35:44 +0200

    JELLYFISHHH

--- Assets/Scripts/JellyMov.cs
+++ Assets/Scripts/JellyMov.cs
@@ -9,14 +9,15 @@ public class JellyMov : MonoBehaviour
     public float velUp = 5.0f;
     public float velDown = 1.5f;
 
-    public float maxDistance = 20.0f;
+    public float maxDistance = 18.0f;
+    private float originalY;
 
     public bool towardsObjective = true; // empieza abajo y sube hasta una dist
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        originalY = transform.position.y;
     }
 
     // Update is called once per frame
@@ -27,6 +28,13 @@ public class JellyMov : MonoBehaviour
 
         if (objective !=null)
         {
+            if (transform.position.y > originalY + maxDistance)
+            {
+                towardsObjective = false;
+            }else if (transform.position.y < originalY )
+            {
+                towardsObjective = true;
+            }
             
             //behaviour
             if (towardsObjective)
